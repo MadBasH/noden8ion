4950bad2cb32ae76fdd42153afc50658bd9eee76
Alperen Bayrakdar <alperen_bayrakdar@hotmail.com>
2025-10-16 16:45:57 +0300

    setup polar

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
+import { checkout, polar, portal } from "@polar-sh/better-auth";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -11,4 +13,25 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "8d5a106f-18d7-45f0-b84a-70661b144123",
+              slug: "pro",
+            }
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    })
+  ]
+
 });
