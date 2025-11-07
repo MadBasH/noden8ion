69c01cc71854bbd1cdb9c4e0e733492adb791626
Alperen Bayrakdar <alperen_bayrakdar@hotmail.com>
2025-11-07 18:10:21 +0300

    node execution

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -2,7 +2,7 @@
 
 import { ErrorView, LoadingView } from "@/components/entity-components";
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import { 
     ReactFlow, 
     applyNodeChanges, 
@@ -23,6 +23,8 @@ import { nodeComponents } from "@/config/node-components";
 import { AddNodeButton } from "./add-node-button";
 import { useSetAtom } from "jotai";
 import { editorAtom } from "../store/atoms";
+import { NodeType } from "@/generated/prisma";
+import { ExecuteWorkflowButton } from "./execute-workflow-button";
 
 
 export const EditorLoading = () => {
@@ -53,6 +55,9 @@ export const Editor = ({workflowId}: {workflowId: string}) => {
         (params: Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
         [],
     );
+    const hasManuelTrigger = useMemo(() =>  {
+        return nodes.some((node) => node.type === NodeType.MANUAL_TRIGGER);
+    }, [nodes]);
 
     return(
         <div className="size-full">
@@ -77,6 +82,11 @@ export const Editor = ({workflowId}: {workflowId: string}) => {
                 <Panel position="top-right">
                     <AddNodeButton />
                 </Panel>
+                {hasManuelTrigger && (
+                    <Panel position="bottom-center">
+                        <ExecuteWorkflowButton workflowId={workflowId}/>
+                    </Panel>
+                )}
             </ReactFlow>
         </div>
     );
--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -5,9 +5,29 @@ import { createTRPCRouter, premiumProcedure, protectedProcedure } from "@/trpc/i
 import { generateSlug } from "random-word-slugs";
 import z from "zod";
 import type { Node, Edge } from "@xyflow/react";
+import { inngest } from "@/inngest/client";
 
 
 export const workflowsRouter = createTRPCRouter({
+    execute: protectedProcedure
+        .input(z.object({id: z.string() }))
+        .mutation(async ({input, ctx}) => {
+            const workflow = await prisma.workflow.findFirstOrThrow({
+                where: {
+                    id: input.id,
+                    userId: ctx.auth.user.id,
+                },
+            });
+
+            await inngest.send({
+                name: "workflows/execute.workflow",
+                data: {workflowId: input.id},
+            });
+
+            return workflow;
+        }),
+
+
     create: premiumProcedure.mutation(({ ctx }) => {
         return prisma.workflow.create({
             data: {
