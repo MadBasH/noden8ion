5ded804d004b0ef92d4af8b9b87fdb600e6d9617
Alperen Bayrakdar <alperen_bayrakdar@hotmail.com>
2025-11-10 18:49:14 +0300

    node templating

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,11 +1,18 @@
 import type { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky, {type Options as KyOptions} from "ky";
+import Handlebars from "handlebars";
+
+Handlebars.registerHelper("json", (context) => {
+    const jsonString = JSON.stringify(context, null, 2);
+    const safeString = new Handlebars.SafeString(jsonString);
+    return safeString;
+});
 
 type HttpRequestData = {
-    variableName?: string;
-    endpoint?: string;
-    method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE" ;
+    variableName: string;
+    endpoint: string;
+    method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE" ;
     body?: string;
 };
 
@@ -21,17 +28,24 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async({
     }
 
     if (!data.variableName) {
-        throw new NonRetriableError("Variable name not configured!");
+        throw new NonRetriableError("HTTP Request node: Variable name not configured!");
+    }
+
+    if (!data.method) {
+        throw new NonRetriableError("HTTP Request node: Method not configured!");
     }
 
     const result = await step.run("http-request", async () => {
-        const endpoint = data.endpoint!;
-        const method = data.method || "GET";
+        const endpoint = Handlebars.compile(data.endpoint)(context);
+        // console.log("ENDPOINT", {endpoint});
+        const method = data.method;
 
         const options: KyOptions = {method};
 
         if (["POST", "PUT", "PATCH"].includes(method)) {
-            options.body = data.body;
+            const resolved = Handlebars.compile(data.body || "{}")(context);
+            JSON.parse(resolved);
+            options.body = resolved;
             options.headers = {
                 "Content-Type" : "application/json",
             };
@@ -51,17 +65,11 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async({
             },
         };
 
-        if (data.variableName) {
-            return {
-                ...context,
-                [data.variableName]: responsePayload,
-            }
-        }
-
+        
         return {
             ...context,
-            ...responsePayload,
-        };
+            [data.variableName]: responsePayload,
+        }
     });
 
     return result;
